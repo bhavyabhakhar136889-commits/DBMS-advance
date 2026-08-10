e2f1ad0ff8b996fd9b18952dee78cd7185d51026
bhavy <bhavya.bhakhar136889@marwadiuniversity.ac.in>
2026-08-10 15:02:31 +0530

    Add files via upload

---
+++ pro13.sql
@@ -0,0 +1,18 @@
+--Write a PL/SQL block that uses a cursor attribute %ROWCOUNT
+to display the name, department and basic salary of the first 5
+employees getting the highest basic salary. (Use Explicit Cursor)
+set serveroutput on
+declare
+Cursor s1 IS select * from emp order by salary desc;
+d emp%ROWTYPE;
+begin
+open s1;
+loop
+fetch s1 INTO d;
+exit when NOT s1%FOUND AND s1%ROWCOUNT > 5;
+dbms_output.put_line('Name:'||d.ename||'
+Department:'||d.deptno||' Salary:'||d.salary);
+end loop;
+close s1;
+end;
+/
